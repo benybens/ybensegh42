ce92edba27a509baa3ed06e716eac1e4b23b51e6
Benseghir <ybensegh@pmintl.net>
2022-10-12 17:30:41 +0200

    process %d and %s format , with multiple occurence

--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -6,15 +6,16 @@
 //
 //description: print str of format until next '%' or next '\' or '\0'
 //-----------------------------------------------------------------------------
-int	print_str(const char *format)
+int	print_str(const char *format, int i)
 {
-	int	i;
 	char	*str;
-	i = 0;
+	int start;
+
+	start = i;
 	while (format[i] && (format[i] != '%'))
 		i++;
-	str = ft_calloc(i, sizeof(char));
-	ft_strlcpy(str, format, i + 1);
+	str = ft_calloc(i-start, sizeof(char));
+	ft_strlcpy(str, format + start, i+1-start );
 	ft_putstr_fd(str, 1);
 	free(str);
 	return (i);
@@ -32,16 +33,36 @@ void	process_int(int nbr)
 }
 
 //-----------------------------------------------------------------------------
+//function name: process_str
+//
+//description: print str 
+//-----------------------------------------------------------------------------
+void	process_str(char *str)
+{
+	ft_putstr_fd(str, 1);
+}
+
+//-----------------------------------------------------------------------------
 //function name: format_parse
 //
 //description: identify printf format
 //-----------------------------------------------------------------------------
-int	format_parse(const char *format, int i)
+int	format_parse(const char *format, int i, va_list *ap)
 {
+	int format_int;
+	char *format_str;
+
 	i++;
 	if(format[i] == 'd')
 	{
-		process_int(15);
+		format_int = va_arg(*ap,int);
+		process_int(format_int);
+		i++;
+	}
+	if(format[i] == 's')
+	{
+		format_str = va_arg(*ap,char *);
+		process_str(format_str);
 		i++;
 	}
 	return (i);
@@ -52,18 +73,20 @@ int	format_parse(const char *format, int i)
 //
 //description: count number of '%' to know the number of ellistic arg
 //-----------------------------------------------------------------------------
-int	countarg(format)
+int	countarg(const char *format)
 {
 	int count;
 	int i;
 
-	i = ;
+	i = 0;
 	count = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
 			count++;
+		i++;
 	}
+	printf("count %d",count);
 	return (count);
 }
 
@@ -76,22 +99,24 @@ int	ft_printf(const char *format, ...)
 {
 	char	*str;
 	int	i;
-	int 	nbarg;
+	va_list	ap;
 
 	i = 0;
+	va_start(ap, format);
 	str = ft_strdup(format);
-	nbarg = countarg(format);
+//	countarg(format);
 	while (str[i])
 	{	
 		if (str[i] == '%')
 		{
-			i = format_parse(format, i);
-			ft_putendl_fd("",1);
-			break;
+			i = format_parse(format, i, &ap);
 		}	
 		else
-			i = print_str(str);
+		{
+			i = print_str(str, i);
+		}
 	}
 	free(str);
+	va_end(ap);
 	return (1);
 }
