58eccdbeee7efa5daf5ae69db3ce22c7192422d9
Yassine Benseghir <yassine.benseghir@outlook.com>
2022-11-17 16:55:24 +0100

    fix compilation problem and start to pass francinette

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: ybensegh <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 16:05:13 by ybensegh          #+#    #+#             */
-/*   Updated: 2022/11/14 16:11:39 by ybensegh         ###   ########.fr       */
+/*   Updated: 2022/11/17 16:09:48 by yassinebenseg    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,8 @@
 
 int main()
 {
-	int nbr;
+	char str[20] = "Hello";
 
-	ft_printf("-----Starting test of libftprintf.a------\n");
-	ft_printf("1/ This is a simple string\n");
-	nbr = 15;
-	ft_printf("2/ This is a string with 1 param, int -> %d\n",nbr);
-	nbr = 0;
-	ft_printf("3/ This is a string with 1 param, str -> %s\n","i am a string param");
-	nbr = -98;
-	ft_printf("4/ This is a string with 1 param, int -> %d\n",nbr);
-	nbr = 124546;
-	ft_printf("5/ This is a string with 1 param, int -> %d\n",nbr);
-	nbr = -90;
-	ft_printf("6/ This is a string with 2 params, int -> %d, int -> %s\n",nbr,":test:");
-	nbr = 42;
-/*	ft_printf("7/ This is a string with 2 params, int -> %d, int -> %d\n",nbr,nbr+2);
-	nbr = 50;
-	ft_printf("8/ This is a string with 2 params, int -> %d, int -> %d\n",nbr,nbr+2);
-	nbr = 60;
-	ft_printf("9/ This is a string with 3 params, int -> %d, int -> %d, int -> %d\n",nbr,nbr+2,nbr+4);
-	nbr = 42;
-	ft_printf("10/ This is a string with 3 params, int -> %d, int -> %d, int -> %d\n",nbr,nbr+2,nbr+4);
-	nbr = 50;
-	ft_printf("11/ This is a string with 3 params, int -> %d, int -> %d, int -> %d\n",nbr,nbr+2,nbr+4);
-	nbr = 60;
-	ft_printf("12/ This is a string with 4 params, str -> %s, str -> %s, str -> %s, str -> %s\n","a","b","c","d");
-*/
-
+	printf("str  = %s , is stored at adress %p\n" , str , str);
+	ft_printf("str  = %s , is stored at adress %p\n" , str, str);
 }
--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ybensegh <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 16:05:07 by ybensegh          #+#    #+#             */
-/*   Updated: 2022/11/14 16:05:18 by ybensegh         ###   ########.fr       */
+/*   Updated: 2022/11/17 16:53:02 by yassinebenseg    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@
 //
 //description: print str of format until next '%' or next '\' or '\0'
 //-----------------------------------------------------------------------------
-int	print_str(const char *format, int i)
+int	print_str(const char *format, int i, int * nbr_printed)
 {
 	char	*str;
 	int start;
 
 	start = i;
 	while (format[i] && (format[i] != '%'))
+	{
 		i++;
+		*nbr_printed = *nbr_printed + 1;
+	}
 	str = ft_calloc(i-start+1, sizeof(char));
 	ft_strlcpy(str, format + start, i+1-start );
 	ft_putstr_fd(str, 1);
@@ -38,9 +41,17 @@ int	print_str(const char *format, int i)
 //
 //description: convert int in ascii and print
 //-----------------------------------------------------------------------------
-void	process_int(int nbr)
+int	process_int(int nbr)
 {
-	ft_putstr_fd(ft_itoa(nbr), 1);
+	char	*itoa_nbr;
+	int	intlen;
+
+	itoa_nbr = ft_itoa(nbr);
+	intlen = ft_strlen(itoa_nbr);
+	ft_putstr_fd(itoa_nbr, 1);
+	free(itoa_nbr);
+	return (intlen);
+	
 }
 
 //-----------------------------------------------------------------------------
@@ -53,52 +64,115 @@ void	process_str(char *str)
 	ft_putstr_fd(str, 1);
 }
 
+int	pointer_len(void * pointer)
+{
+	int i;
+	long pointeradd;
+
+	pointeradd = (long)pointer;
+	i = 0;
+	while(pointeradd > 16)
+	{
+		pointeradd /= 16;
+		i++;
+	}
+	if(pointeradd > 0)
+		i++;
+	return(i);
+
+}
+//-----------------------------------------------------------------------------
+//function name: process_str
+//
+//description: convert pointer value into str
+//-----------------------------------------------------------------------------
+char * ft_ptoa(void * pointer)
+{
+	char	*str;
+
+	str = ft_calloc(pointer_len(pointer) + 1,sizeof(pointer));
+	ft_putstr_fd(str, 1);
+	return (str);
+}
+
+//-----------------------------------------------------------------------------
+//function name: process_str
+//
+//description: print str 
+//-----------------------------------------------------------------------------
+void	process_pointer(void * pointer)
+{
+	char	*str;
+
+	str = ft_ptoa(pointer);
+	ft_putstr_fd(str, 1);
+}
+
+
+//-----------------------------------------------------------------------------
+//function name: process_str
+//
+//description: print char
+//-----------------------------------------------------------------------------
+void	process_char(char c)
+{
+	char str[2];
+
+	str[0] = (unsigned char)c;
+	str[1] = 0;
+
+	ft_putstr_fd(str, 1);
+}
 //-----------------------------------------------------------------------------
 //function name: format_parse
 //
 //description: identify printf format
 //-----------------------------------------------------------------------------
-int	format_parse(const char *format, int i, va_list *ap)
+int	format_parse(const char *format, int i, va_list *ap, int * nbr_printed)
 {
-	int format_int;
 	char *format_str;
 
 	i++;
 	if(format[i] == 'd')
 	{
-		format_int = va_arg(*ap,int);
-		process_int(format_int);
+		*nbr_printed += process_int(va_arg(*ap,int));
 		i++;
 	}
-	if(format[i] == 's')
+	else if(format[i] == 's')
 	{
 		format_str = va_arg(*ap,char *);
-		process_str(format_str);
+		if(format_str)
+		{
+			process_str(format_str);
+			*nbr_printed += ft_strlen(format_str);
+		}
+		else
+		{
+			ft_putstr_fd("(null)",1);
+			*nbr_printed += ft_strlen("(null)");
+		}
+		i++;
+	}
+	else if(format[i] == 'c')
+	{
+		process_char(va_arg(*ap,int));
+		*nbr_printed = *nbr_printed + 1;
+		i++;
+	}
+	else if(format[i] == '%')
+	{
+		ft_putstr_fd("%",1);
+		*nbr_printed = *nbr_printed + 1;
+		i++;
+	}
+	else if(format[i] == 'p')
+	{
+		ft_putstr_fd("0x",1);
+		*nbr_printed = *nbr_printed + 2;
+		process_pointer(va_arg(*ap,void *));
 		i++;
 	}
 	return (i);
-}
-
-//-----------------------------------------------------------------------------
-//function name: countarg
-//
-//description: count number of '%' to know the number of ellistic arg
-//-----------------------------------------------------------------------------
-int	countarg(const char *format)
-{
-	int count;
-	int i;
-
-	i = 0;
-	count = 0;
-	while (format[i])
-	{
-		if (format[i] == '%')
-			count++;
-		i++;
-	}
-	printf("count %d",count);
-	return (count);
 }
 
 //-----------------------------------------------------------------------------
@@ -112,8 +186,12 @@ int	ft_printf(const char *format, ...)
 	int	i;
 	va_list	ap;
 	int len;
+	int nbr_printed;
 
 	i = 0;
+	nbr_printed = 0;
+	if (format == NULL)
+		return (0);
 	va_start(ap, format);
 	str = ft_strdup(format);
 	len = ft_strlen(str);
@@ -122,14 +200,14 @@ int	ft_printf(const char *format, ...)
 	{	
 		if (str[i] == '%')
 		{
-			i = format_parse(format, i, &ap);
+			i = format_parse(format, i, &ap, &nbr_printed);
 		}	
 		else
 		{
-			i = print_str(str, i);
+			i = print_str(str, i, &nbr_printed);
 		}
 	}
 	free(str);
 	va_end(ap);
-	return (1);
+	return (nbr_printed);
 }
