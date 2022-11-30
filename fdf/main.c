65d43fe326ce6eb7332f8b860f9eb166b48b1f52
benybens <88706889+benybens@users.noreply.github.com>
2022-11-30 17:12:53 +0100

    first trace

--- fdf/main.c
+++ fdf/main.c
@@ -6,20 +6,43 @@
 /*   By: yassinebenseghir <marvin@42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 14:57:39 by yassinebenseg     #+#    #+#             */
-/*   Updated: 2022/11/30 15:12:01 by yassinebenseg    ###   ########.fr       */
+/*   Updated: 2022/11/30 16:46:18 by yassinebenseg    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "ft_printf.h"
 
+
+int deal_key(int keycode, void *param)
+{
+	(void)keycode;
+	(void)param;
+	ft_printf("Key event\n");
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
+	void *mlx_ptr;
+	void *win_ptr;
+	int i;
+
 	(void) argv;
-	if(argc != 2)
+	if (argc != 2)
 	{
 		ft_printf("Please run : ./fdf 'map_path'\n");
-		return(0);
+		return (0);
 	}
 	ft_printf("Start fdf\n");
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Fdf window");
+	i = 0;
+	while (i < 500)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, 250, i,0x43C0A3);
+		i++;
+	}
+	mlx_key_hook(win_ptr,deal_key,(void *)0);
+	mlx_loop(mlx_ptr);
 }
